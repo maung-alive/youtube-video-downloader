67245c2458ed087c1e9d14ee44cdde28e87fb466
Maung <maung.alive@gmail.com>
2024-01-21 09:07:40 +0630

    Command Line Setup

--- helpers/main.py
+++ helpers/main.py
@@ -1,4 +1,5 @@
-import argparse
+import argparse, sys, requests, humanize
+from parseURL import parseURL
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-u', '--url', dest='url', type=str, required=True,
@@ -11,5 +12,35 @@ parser.add_argument('-v', dest='verbose', action='store_true',
                     help='Verbose mode')
 
 args = parser.parse_args()
-print(args.url)
 
+def progess_bar(current, total):
+    bar_length = 25
+    percent = float(current) / float(total)
+    hashes = '#' * int(round(percent * bar_length))
+    spaces ='_' * (bar_length - len(hashes))
+    sys.stdout.write(f'\r{current}/{total} bytes   [{hashes}{spaces}] {int(round(percent * 100))}%')
+    sys.stdout.flush()
+
+def main(): 
+    data = parseURL(args.url)
+    print("Video", data['title'])
+
+    key = 0
+    for i in data['files']:
+        print(f"[{key}]", i['quality'], i['type'], ' - ', i['size'])
+        key += 1
+    
+    resolution = input('[+] Resolution: ')
+    file = data['files'][int(resolution)]
+    response = requests.get(file['url'], stream=True)
+
+    total = int(response.headers['Content-length'])
+    current = 0
+    with open(f"{data['title']}.{file['type']}", "wb") as handle:
+        for data in response.iter_content(chunk_size=1024):
+            progess_bar(current, total)
+            handle.write(data)
+            current += 1024
+
+if __name__ == "__main__":
+    main()
